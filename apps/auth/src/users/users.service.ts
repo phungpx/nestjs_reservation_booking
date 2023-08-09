1566f6c07bf8ce65d8820d143d52e47a4ac7bd02
phungpx <phamxuanphungbku@gmail.com>
2023-08-09 09:33:47 +0700

    update local strategies

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly usersRepository: UsersRepository){}
+  constructor(private readonly usersRepository: UsersRepository) {}
 
-    async create(createUserDto: CreateUserDto){
-        return this.usersRepository.create(createUserDto);         
+  async create(createUserDto: CreateUserDto) {
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10), // overwrite the password
+    });
+  }
+
+  async validateCreateUserDto(createUserDto: CreateUserDto) {
+    try {
+      await this.usersRepository.findOne({ email: createUserDto.email });
+    } catch (err) {
+      return;
     }
+    throw new UnprocessableEntityException('Email already exists.');
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+    return user;
+  }
+
+  //   async getUser(getUserDto: GetUserDto){}
 }
